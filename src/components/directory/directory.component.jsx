010536f26e72fbe5b0085e159bec82a2650f0fda
unknown <the25store@gmail.com>
2024-11-10 13:19:43 -0800

    first commit

--- src/components/directory/directory.component.jsx
+++ src/components/directory/directory.component.jsx
@@ -1,14 +1,14 @@
 import {DirectoryContainer } from './directory.styles';
 
 import DirectoryItem from '../directory-item/directory-item.component';
-import Second from '../second/second.component';
-import Third from '../third/third.component';
+// import Second from '../second/second.component';
+// import Third from '../third/third.component';
 
 const Directory = () => {
     return (
         <DirectoryContainer>
-        {/* <DirectoryItem/>
-        <Second/> */}
+        <DirectoryItem/>
+        {/* <Second/> */}
         {/* <Third/> */}
        </DirectoryContainer>
     );
