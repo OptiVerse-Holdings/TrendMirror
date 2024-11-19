1a3b92df86f04393527cef628625eb8f43e42aa5
d25 <oladokundara@gmail.com>
2024-11-19 10:31:10 -0800

    first commit

--- src/components/directory/directory.component.jsx
+++ src/components/directory/directory.component.jsx
@@ -2,14 +2,16 @@ import {DirectoryContainer } from './directory.styles';
 
 import DirectoryItem from '../directory-item/directory-item.component';
 import Second from '../second/second.component';
-// import Third from '../third/third.component';
+import Third from '../third/third.component';
+import  Fourth from '../fourth/fourth.component';
 
 const Directory = () => {
     return (
         <DirectoryContainer>
         <DirectoryItem/>
         <Second/>
-      {/* <Third/> */}
+        <Third/>
+        <Fourth/>
        </DirectoryContainer>
     ); 
 };
